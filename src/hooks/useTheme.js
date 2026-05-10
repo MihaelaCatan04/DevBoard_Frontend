7620fb889dfb9d417bb3a9b33b5e85ae31a26da0
Mihaela Catan <66206241+MihaelaCatan04@users.noreply.github.com>
2026-05-10 09:40:52 +0300

    fix: correct theme to not be related to the os theme prefernces (#12)

--- src/hooks/useTheme.js
+++ src/hooks/useTheme.js
@@ -4,7 +4,7 @@ export function useTheme() {
   const [isDark, setIsDark] = useState(() => {
     const stored = localStorage.getItem("devboard_theme");
     if (stored) return stored === "dark";
-    return window.matchMedia("(prefers-color-scheme: dark)").matches;
+    return true;
   });
 
   useEffect(() => {
